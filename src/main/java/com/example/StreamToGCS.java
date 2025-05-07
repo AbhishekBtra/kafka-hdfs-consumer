36b4124a4007c2bf04d69983952a368e2965599d
Abhishek Batra <abatra002@gmail.com>
2025-05-07 19:28:26 +0530

    add comments

--- src/main/java/com/example/StreamToGCS.java
+++ src/main/java/com/example/StreamToGCS.java
@@ -8,16 +8,19 @@ import com.google.cloud.storage.*;
 
 public class StreamToGCS {
     
+    //function to send records to GCS bucket
     public static void sendRowsToGcs(String records) throws StorageException{
 
         String bucketName = "buck81";
         Random rn = new Random();
         String blobName = "metadata_"+rn.nextInt();
 
-
+        //get storage
         Storage storage = StorageOptions.getDefaultInstance().getService();
+        //prepare blob
         BlobInfo bI = BlobInfo.newBuilder(bucketName,blobName).setContentType("text-plain").build();
-        BlobId bId = BlobId.of(bucketName, blobName);
+        //BlobId bId = BlobId.of(bucketName, blobName);
+        //create blob
         storage.create(bI,records.getBytes(StandardCharsets.UTF_8));
     }
 }
